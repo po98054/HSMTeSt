f4be4c634d2a518bc955e77358c3867ab862bc35
ddd <po98054@naver.com>
2022-10-25 20:50:58 +0900

    수업 정리

---
+++ src/Day02/operator04.java
@@ -0,0 +1,36 @@
+package Day02;
+
+public class operator04 {
+
+	public static void main(String[] args) {
+		
+		int num = 20;
+		int num2 = 30;
+		
+		boolean bo1 = num < 30 && num2 > 40; // false (20 < 30 true 30 > 40 false)
+		boolean bo2 = num < 30 || num2 > 40; // true
+		
+		System.out.println(bo1);
+		System.out.println(bo2);
+		
+		int score = 85;
+		
+		boolean isB = score >= 80 && score < 90; // 85 >= 80 true 85 < 90 true
+		
+		System.out.println(isB);
+		
+		num += num;
+		
+		System.out.println(num); //40
+		
+		String result = score >= 60 ? "통과" : "탈락";
+		
+		System.out.println(result);
+		
+		int max = num > num2 ? num2 : num;
+		
+		System.out.println(max);
+		
+	}
+
+}
