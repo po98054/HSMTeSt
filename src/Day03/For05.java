f7b406267136796948fa9072d781379938d4c0d7
ddd <po98054@naver.com>
2022-10-26 21:50:15 +0900

    For문 합계 수정

--- src/Day03/For05.java
+++ src/Day03/For05.java
@@ -4,9 +4,9 @@ public class For05 {
 
 	public static void main(String[] args) {
 		
-		int sum = 0;
+		int i, sum;
 		
-		for (int i = 0; i <= 10; i++) {
+		for (i = 1, sum = 0; i <= 10; i++) {
 			
 			sum = sum + i;
 		}
