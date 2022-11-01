b9d0c51e673717dd8a3a76e537d62fff64b3aebc
po98054 <po98054@naver.com>
2022-11-01 17:27:10 +0900

    Update Method01.java
    강사님이 알려주신 소스까지 합친것임.

--- Week02/Day07/Method01.java
+++ Week02/Day07/Method01.java
@@ -16,6 +16,23 @@ public class Method01 {
 			
 		}
 		
+		System.out.println("====================================");
+		
+		gogudan4(5,1,9);
+		
+		System.out.println("====================================");
+		
+		for(int i = 2; i < 10; i++) {
+			
+			for(int j = 1; j < 10; j++) {
+				
+				System.out.println(i + " * " + j + " = " + gogudan3(i,j));
+			}
+			
+			System.out.println("====================================");
+			
+		}
+		
 	}
 	
 	// 구구단을 출력하는 메소드
@@ -31,9 +48,31 @@ public class Method01 {
 		
 	}
 	
-	public static int gogudan2(int num1) {
+	// 2.
+	
+	public static int gogudan2(int num2) {
 		
-		return 7 * num1;
+		return 7 * num2;
+		
+	}
+	
+	// 2. 강사님이 알려주신 방법
+	
+	public static void gogudan4(int num, int start, int end) {
+		
+		for(int i = start; i <= end; i++) {
+			
+			System.out.println(num + " * " + i + " = " + (num*i)); 
+			
+		}
+		
+	}
+	
+	// 3.
+	
+	public static int gogudan3(int num, int num2) {
+		
+		return num * num2;
 		
 	}
 
