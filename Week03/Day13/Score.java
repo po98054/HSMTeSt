09ea644d562b421e5f95b6826325fbf3104ea8c5
po98054 <po98054@naver.com>
2022-11-09 19:05:00 +0900

    학생정보관리프로그램 Ver1

---
+++ Week03/Day13/Score.java
@@ -0,0 +1,178 @@
+package Day13;
+
+public class Score {
+	
+	// 필드
+	
+	private String sub;
+	
+	private int dict;
+	
+	private int middle;
+	
+	private int fin;
+	
+	private int perfor;
+	
+	private double total;
+
+	// 메소드 : equals() <= 성적 추가시 활용 toString <= 성적 출력시 활용 getter/setter
+	
+	// getter/setter
+	
+	public String getSub() {
+		
+		return sub;
+		
+	}
+
+	public void setSub(String sub) {
+		
+		this.sub = sub;
+		
+	}
+
+	public int getDict() {
+		
+		return dict;
+		
+	}
+
+	public void setDict(int dict) {
+		
+		this.dict = dict;
+		
+	}
+
+	public int getMiddle() {
+		
+		return middle;
+		
+	}
+
+	public void setMiddle(int middle) {
+		
+		this.middle = middle;
+		
+	}
+
+	public int getFin() {
+		
+		return fin;
+		
+	}
+
+	public void setFin(int fin) {
+		
+		this.fin = fin;
+		
+	}
+
+	public int getPerfor() {
+		
+		return perfor;
+		
+	}
+
+	public void setPerfor(int perfor) {
+		
+		this.perfor = perfor;
+		
+	}
+	
+	public double getTotal() {
+		
+		return total;
+		
+	}
+
+	public void setTotal(double total) {
+		
+		this.total = total;
+		
+	}
+	
+	// equals
+
+	@Override
+	
+	public boolean equals(Object obj) {
+		
+		if (this == obj)
+			
+			return true;
+		
+		if (obj == null)
+			
+			return false;
+		
+		if (getClass() != obj.getClass())
+			
+			return false;
+		
+		Score other = (Score) obj;
+		
+		if (dict != other.dict)
+			
+			return false;
+		
+		if (sub == null) {
+			
+			if (other.sub != null)
+				
+				return false;
+			
+		} else if (!sub.equals(other.sub))
+			
+			return false;
+		
+		return true;
+		
+	}
+	
+	// toString
+
+	@Override
+	
+	public String toString() {
+		
+		return sub + "의 " + dict + "학기의 중간 성적은 " + middle + " " + dict + "학기의 기말 성적은 " + fin + " " + dict + "학기의 수행 평가 성적은 " + perfor + "입니다.";
+		
+	}
+	
+
+	// 생성자 : 과목 학기 중간 기말 수행평가가 필요한 생성자
+
+	public Score(String sub, int dict, int middle, int fin, int perfor) {
+		
+		this.sub = sub;
+		
+		this.dict = dict;
+		
+		this.middle = middle;
+		
+		this.fin = fin;
+		
+		this.perfor = perfor;
+		
+	}
+	
+	// 과목 학기가 필요한 생성자 <= 성적이 같은지 확인할 때
+	
+	public Score(String sub, int dict) {
+		
+		this.sub = sub;
+		
+		this.dict = dict;
+		
+	}
+	
+	// 복사 생성자 
+	
+	public Score(Score s) {
+		
+		this(s.sub, s.dict, s.middle, s.fin, s.perfor);
+		
+	}
+
+}
---
+++ Week03/Day13/Student.java
@@ -0,0 +1,202 @@
+package Day13;
+
+public class Student {
+	
+	// 필드
+	
+	private int grade;
+	
+	private int ban;
+	
+	private int bunho;
+	
+	private String name;
+	
+	private Score scoreList[];
+	
+	private int maxSize; // 최대 저장 가능한 성적 수
+	
+	private int count; // 현재 저장된 성적 수
+	
+	// 각 학생 별 성적을 추가
+	// 추가할 성적 추가 여부
+	
+	public boolean addScore(Score score) {
+		
+		if(score == null) {
+			
+			return false;
+			
+		}
+		
+		// 배열에 성적들이 꽉 찬 경우
+		
+		if(count == maxSize) {
+			
+			return false;
+			
+		}
+		
+		for(int i = 0; i < count; i++) {
+			
+			if(score.equals(scoreList[i])) {
+				
+				return false;
+				
+			}
+			
+		}
+		
+		scoreList[count] = new Score(score);
+		
+		count = count + 1;
+		
+		return true;
+		
+	}
+
+	// 메소드 : getter/setter , equals() : 같은 객체 비교 여부, toString() : 객체를 쉽게 문자열 생성
+	
+	// getter/setter
+	
+	public int getGrade() {
+		
+		return grade;
+		
+	}
+
+	public void setGrade(int grade) {
+		
+		this.grade = grade;
+		
+	}
+
+	public int getBan() {
+		
+		return ban;
+		
+	}
+
+	public void setBan(int ban) {
+		
+		this.ban = ban;
+		
+	}
+
+	public int getBunho() {
+		
+		return bunho;
+		
+	}
+
+	public void setBunho(int bunho) {
+		
+		this.bunho = bunho;
+		
+	}
+
+	public String getName() {
+		
+		return name;
+		
+	}
+
+	public void setName(String name) {
+		
+		this.name = name;
+		
+	}
+	
+	// equals
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (this == obj)
+			
+			return true;
+		
+		if (obj == null)
+			
+			return false;
+		
+		if (getClass() != obj.getClass())
+			
+			return false;
+		
+		Student other = (Student) obj;
+		
+		if (ban != other.ban)
+			
+			return false;
+		
+		if (bunho != other.bunho)
+			
+			return false;
+		
+		if (grade != other.grade)
+			
+			return false;
+		
+		return true;
+		
+	}
+
+	// toString()
+	
+	@Override
+	public String toString() {
+		
+		return grade + "학년 " + ban + "반 " + bunho + "번 " + name + "입니다.";
+		
+	}
+	
+	// printScore
+	
+	public void printScore() {
+		
+		for(int i = 0; i < count; i++) {
+			
+			System.out.println(scoreList[i]);
+			
+		}
+		
+	}
+	
+	// 생성자 : 초기화, 
+	
+	// 학년, 반, 번호, 이름이 필요한 생성자 <= 학생 정보 추가시 활용
+	
+	public Student(int grade, int ban, int bunho, String name) {
+		
+		this.grade = grade;
+		
+		this.ban = ban;
+		
+		this.bunho = bunho;
+		
+		this.name = name;
+		
+		maxSize = 30;
+		
+		scoreList = new Score[maxSize];
+		
+	}
+
+	// 학년, 반, 번호가 필요 <= 성적 추가 시 학생이 있는지 없는지 판별할 때 활용
+	
+	public Student(int grade, int ban, int bunho) {
+		
+		this.grade = grade;
+		
+		this.ban = ban;
+		
+		this.bunho = bunho;
+		
+		maxSize = 30;
+		
+		scoreList = new Score[maxSize];
+		
+	}
+
+}
